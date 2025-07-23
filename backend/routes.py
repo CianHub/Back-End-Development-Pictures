d04f3b62b79d3f5b1a911a2a71a652bdc1982262
cianhub <ihatecian@gmail.com>
2025-07-23 14:21:04 -0400

    implemented pictures service

--- backend/routes.py
+++ backend/routes.py
@@ -35,7 +35,10 @@ def count():
 ######################################################################
 @app.route("/picture", methods=["GET"])
 def get_pictures():
-    pass
+    if data:
+        return jsonify(data), 200
+
+    return {"message": "Internal server error"}, 500 
 
 ######################################################################
 # GET A PICTURE
@@ -44,7 +47,12 @@ def get_pictures():
 
 @app.route("/picture/<int:id>", methods=["GET"])
 def get_picture_by_id(id):
-    pass
+    if data and type(id) == int:
+        for image in data:
+            if image["id"] == id:
+                return jsonify(image), 200
+
+    return {"message": "Not Found"}, 404
 
 
 ######################################################################
@@ -52,7 +60,17 @@ def get_picture_by_id(id):
 ######################################################################
 @app.route("/picture", methods=["POST"])
 def create_picture():
-    pass
+    picture = request.get_json()
+
+    if picture not in data:
+        data.append(picture)
+        return jsonify(picture), 201
+    elif picture in data:
+        return {"Message": f"picture with id {picture['id']} already present"}, 302
+ 
+
+    return {"message": "Internal server error"}, 500 
+
 
 ######################################################################
 # UPDATE A PICTURE
@@ -61,11 +79,28 @@ def create_picture():
 
 @app.route("/picture/<int:id>", methods=["PUT"])
 def update_picture(id):
-    pass
+    picture = request.get_json()
+
+    for p in data:
+        if p["id"] == id:
+            p["pic_url"] = picture.get("pic_url", p.get("pic_url"))
+            p["event_country"] = picture.get("event_country", p.get("event_country"))
+            p["event_state"] = picture.get("event_state", p.get("event_state"))
+            p["event_city"] = picture.get("event_city", p.get("event_city"))
+            p["event_date"] = picture.get("event_date", p.get("event_date"))
+            return jsonify(p), 200
+
+    return {"message": "picture not found"}, 404
+
 
 ######################################################################
 # DELETE A PICTURE
 ######################################################################
 @app.route("/picture/<int:id>", methods=["DELETE"])
 def delete_picture(id):
-    pass
+    for p in data:
+        if p["id"] == id:
+            data.remove(p)
+            return {}, 204
+    return {"message": "picture not found"}, 404
+
